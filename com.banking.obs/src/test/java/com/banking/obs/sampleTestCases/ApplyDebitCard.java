11d029f55d7475a9d462d9573c1ee013586e0d49
subra <subra@DESKTOP-HGCGENQ>
2023-08-17 11:19:39 +0530

    apply debit card class added

---
+++ com.banking.obs/src/test/java/com/banking/obs/sampleTestCases/ApplyDebitCard.java
@@ -0,0 +1,21 @@
+package com.banking.obs.sampleTestCases;
+
+import org.testng.annotations.Test;
+
+public class ApplyDebitCard {
+	
+	
+	@Test
+	public void getActiveAccount() {
+		
+		System.out.println("----fetch active account ----");
+	}
+	
+	@Test
+	public void applyDebitCard()
+	{
+		
+		System.out.println(" ---apply debit card ----");
+	}
+
+}
