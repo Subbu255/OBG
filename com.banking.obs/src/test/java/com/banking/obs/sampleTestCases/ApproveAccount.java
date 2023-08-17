8993e626b675640170bdadf7f787258c850ae2c0
subra <subra@DESKTOP-HGCGENQ>
2023-08-17 11:31:34 +0530

    approve account is added

---
+++ com.banking.obs/src/test/java/com/banking/obs/sampleTestCases/ApproveAccount.java
@@ -0,0 +1,14 @@
+package com.banking.obs.sampleTestCases;
+
+import org.testng.annotations.Test;
+
+public class ApproveAccount {
+	
+	
+	@Test
+	public void approveAccount() {
+		
+		System.out.println("---approve account ----");
+	}
+
+}
